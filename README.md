6fa3cfb52ef3254e5d0060bc26f2df7ff6b08abb
Matias Sendra <mjs.sendra@gmail.com>
2022-09-02 17:55:40 -0300

    Primer commit

---
+++ README.md
@@ -0,0 +1,8 @@
+# Servidor Web creado con Node JS + HTTP + Express + Handlebars
+
+``npm install``
+``nodemon app``
+
+Esta aplicación es parte de mi curso de Node de cero a experto que puedes encontrar aquí:
+
+[Node: De cero a experto](https://fernando-herrera.com/#/curso/node-cero-experto)
---
+++ app.js
@@ -0,0 +1,48 @@
+const express = require('express')
+const hbs = require('hbs');
+
+const app = express()
+const port = 8082;
+
+
+// Motor de plantillas Handlebars
+app.set('view engine', 'hbs');
+hbs.registerPartials(__dirname + '/views/partials');
+
+
+
+// Servir contenido estático
+app.use(express.static('public'));
+
+// Esto se renderiza con Handlebars
+app.get('/', (req, res) => {
+    res.render('home', {
+        nombre: 'Fernando Herrera',
+        titulo: 'Curso de Node'
+    });
+});
+
+// Ruta 2
+app.get('/generic', (req, res) => {
+    res.render('generic', {
+        nombre: 'Fernando Herrera',
+        titulo: 'Curso de Node'
+    });
+});
+
+// Ruta 2
+app.get('/elements', (req, res) => {
+    res.render('elements', {
+        nombre: 'Fernando Herrera',
+        titulo: 'Curso de Node'
+    });
+});
+
+// Ruta 3
+app.get('*', (req, res) => {
+    res.sendFile(__dirname + '/public/404.html')
+});
+
+app.listen(port, () => {
+    console.log(`Example app listening on port ${port}`)
+})
